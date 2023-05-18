7e18cc82bdf845f8a3b2c08276fe71a8260aa941
ccaampss <annacampsromero@gmail.com>
2023-05-18 15:09:57 +0300

    no va el css

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
 const Chats = () => {
-    return(
-        <div>Chats</div>
+    return (
+        <div className='chats'>
+            <div className="userChat">
+                <img src="https://randomuser.me/api/portraits/women/90.jpg" alt="user" />
+                <div className='userChatInfo'>
+                    <span>Anna</span>
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://randomuser.me/api/portraits/women/90.jpg" alt="user" />
+                <div className='userChatInfo'>
+                    <span>Anna</span>
+                    <p>hello</p>
+                </div>
+            </div>
+            <div className="userChat">
+                <img src="https://randomuser.me/api/portraits/women/90.jpg" alt="user" />
+                <div className='userChatInfo'>
+                    <span>Anna</span>
+                    <p>hello</p>
+                </div>
+            </div>
+        </div>
+
     )
 }
-        
+
 export default Chats;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Attach from '../img/attach.png';
+import Img from '../img/img.png';
 
 const Input = () => {
-    return(
-        <div>Input</div>
+    return (
+        <div className='input' placeholder='Type something...'>
+            <div className='send'>
+                <img src={Attach} alt="" />
+                <input type='file' style={{ display: 'none' }} id='file' />
+                <label htmlfor='file'>
+                    <img src={Img} alt="" />
+                </label>
+                <button>Send</button>
+            </div>
+
+        </div>
     )
 }
-        
+
 export default Input;
